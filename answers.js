b4e43a331fdceb1b99b490b6214b667d00278a5d
Andrei Makrushin <andrei18makrushin@gmail.com>
2023-01-15 17:39:01 +0500

    module stryctyre

---
+++ answers.js
@@ -0,0 +1,112 @@
+export const  questions = [
+    {
+        question: "Name the group in the photo",
+        img: "https://channel-korea.com/wp-content/uploads/2018/03/gfriend-0.jpg",
+        answers: [
+            "AOA", 
+            "RED VELVET", 
+            "MAMAMOO",
+            "GFRIEND"
+        ],
+        correct: 4,
+    },
+    {
+        question: "What K-POP group is in the picture?",
+        img: "https://chpic.su/_data/stickers/b/BKillThisLoveP/BKillThisLoveP_016.webp",
+        answers: [
+            "MAMAMOO", 
+            "BLACKPINK", 
+            "TWICE",
+            "(G)I-DLE"
+        ],
+        correct: 2,
+    },
+    {
+        question: "What K-POP group is in the picture?",
+        img: "https://lh3.googleusercontent.com/VGR4nsugxwRVM22cNW-pAVrBYGgMN3ZiGFgabbmQPj848geQ0HZRID9q7CwDtConxfo6VZPG3wkmappASHLGCg_j7Ud6h2JR=w1600-rj",
+        answers: [
+            "TWICE", 
+            "(G)I-DLE", 
+            "SNSD",
+            "Red Velvet"
+        ],
+        correct: 1,
+    },
+    {
+        question: "What K-POP group is in the picture?",
+        img: "https://0.soompi.io/wp-content/uploads/2019/02/25011223/ITZY-12.jpg",
+        answers: [
+            "Red Velvet", 
+            "ITZY", 
+            "TWICE",
+            "SNSD"
+        ],
+        correct: 2,
+    },
+    {
+        question: "What K-POP group is in the picture?",
+        img: "https://6.viki.io/image/6d47a9fd4be94646896eef44505777eb.jpeg?s=900x600&e=t",
+        answers: [
+            "(G)I-DLE", 
+            "SNSD", 
+            "IZ*ONE",
+            "BlackPink"
+        ],
+        correct: 1,
+    },
+    {
+        question: "Which album never existed?",
+        img: "",
+        answers: [
+            "GOT7 Album Vol. 4 — Breath Of Love", 
+            "MOMOLAND Single Album Vol. 3 — Ready Or Not", 
+            "NCT — The 2nd Album RESONANCE",
+            "SEVENTEEN Album Vol. 2 — Exit"
+        ],
+        correct: 4,
+    },
+    {
+        question: "What band's lightstick is in the photo?",
+        img: "https://cdn.shopify.com/s/files/1/2420/2037/products/gidle_700x.jpg?v=1573936567",
+        answers: [
+            "BlackPink", 
+            "IZ*ONE", 
+            "TWICE",
+            "(G)I-DLE"
+        ],
+        correct: 4,
+    },
+    {
+        question: "In what year did Girls' Generation officially debut?",
+        img: "https://get.wallhere.com/photo/2560x1600-px-Girls-Generation-hyoyeon-jean-shorts-Jessica-Jung-K-pop-kim-taeyon-Kwon-Yuri-Musicians-Seohyun-singer-SNSD-Sooyoung-Sunny-Tiffany-Hwang-white-clothing-Yoona-1086309.jpg",
+        answers: [
+            "2007", 
+            "2008", 
+            "2009",
+            "2010"
+        ],
+        correct: 1,
+    },
+    {
+        question: "What label did BLACKPINK debut under?",
+        img: "",
+        answers: [
+            "SM Entertainment", 
+            "YG Entertainment", 
+            "JYP Entertainment",
+            "Kakao M"
+        ],
+        correct: 2,
+    },
+    {
+        question: "What was the final single of 2017 for the Wonder Girls?",
+        img: "https://images6.fanpop.com/image/polls/1645000/1645993_1503153610087_full.jpg",
+        answers: [
+            "Baby Dont Play", 
+            "Draw Me", 
+            "The DJ is Mine",
+            "Like This"
+        ],
+        correct: 2,
+    }
+]
